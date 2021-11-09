23ed40edc383d52643bb48413f3a0659d10d838d
Hsin <Junette.Hsin@sslmda.com>
2021-11-09 15:54:33 -0600

    finished set 4 prob 1 exam 2 version

--- set4/set4_prob1.m
+++ set4/set4_prob1.m
@@ -1,27 +1,54 @@
+%% set 4, prob 1 
+
 clear; clc
 
-J_fn = @(x) ... 
-    [ 1+x(2),   1+x(1)      ; 
-      2*x(1),   2-2*x(2)    ];
-
+% nonlinear equations f1 and f2  
 fn = @(x) ... 
     [ x(1) + x(2) + x(1)*x(2) + 5   ; 
       x(1)^2 + 2*x(2) - x(2)^2 - 2  ]; 
 
+% jacobian of f1 and f2 
+J_fn = @(x) ... 
+    [ 1+x(2),   1+x(1)      ; 
+      2*x(1),   2-2*x(2)    ];
+
+  
+%% initial conditions 
+
 % first guess 
+xg = [4; -4]; 
+fprintf('\n\n FIRST GUESS: xg = %g, %g \n', xg(1), xg(2)); 
+fprintf('initial guess f norm = %g \n', norm(fn(xg))) 
+
+iter_6(xg, J_fn, fn)
+
+% second guess 
+xg = [6; 0]; 
+fprintf('\n\n SECOND GUESS: xg = %g, %g \n', xg(1), xg(2)); 
+fprintf('initial guess f norm = %g \n', norm(fn(xg))) 
+
+iter_6(xg, J_fn, fn)
+
+% third guess 
 xg = [-5; 5]; 
-fprintf('initial guess f norm') 
-norm(fn(xg))
+fprintf('\n\n THIRD GUESS: xg = %g, %g \n', xg(1), xg(2)); 
+fprintf('initial guess f norm = %g \n', norm(fn(xg))) 
+
+iter_6(xg, J_fn, fn)
+
+%% subfunctions 
 
 % first 6 iterates 
-for i = 1:6 
-    
-    fprintf('\n iter %d \n', i); 
-    fprintf('xg = \n'); 
-    xg = xg - inv(J_fn(xg)) * fn(xg); 
-    disp(xg); 
-    fprintf('f norm = '); 
-    disp((norm(fn(xg))))
 
+function iter_6(xg, J_fn, fn)
+    for i = 1:6 
+
+        fprintf('\n ITER %d \n\n', i); 
+        fprintf('xg = \n'); 
+        xg = xg - inv(J_fn(xg)) * fn(xg); 
+        disp(xg); 
+        fprintf('f norm = %g \n', norm(fn(xg))); 
+
+    end 
 end 
 
