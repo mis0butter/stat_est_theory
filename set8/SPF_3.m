65378a98ad1309a32ce5fb5353ca0795197f003e
Junette Hsin <junetter@gmail.com>
2021-12-06 01:24:04 -0600

    finished creating updating plot, mostly done with SPF_3

--- set8/SPF_3.m
+++ set8/SPF_3.m
@@ -1,4 +1,4 @@
-
+close all; 
 clear; 
 % clc
 
@@ -21,8 +21,8 @@ R = diag( [2*pi/180, 2*pi/180, 0.1] )^2;
 x0 = [90; 4.25; 13; pi; 5; 2]; 
 P0 = diag([2 5 1 pi/4 4 2])^2; 
 
-xa = [ x0; zeros(6,1) ]; 
-Pa = [ P0, zeros(6); zeros(6), Q ]; 
+x_hat = x0; 
+P     = P0; 
 
 % for lambda 
 a = 10^-3; 
@@ -34,6 +34,28 @@ nv = 6;
 
 % load LIDAR data 
 load problem3data.mat 
+N = length(lidar); 
+
+% initialize output arrays 
+x_hat_arr = [x_hat']; 
+P_cell = {P}; 
+
+% create plot 
+fname = 'Car Plot'; 
+h = figure('name', fname); 
+    xlim([0 100]); ylim([-50 50]); 
+    plotcar(x_hat, '-', h)
+%     pause(0.05)
+%     drawnow 
+
+%% UNSCENTED KALMAN FILTER 
+
+% iterate through measurements 
+for j = 1 : N 
+
+% augmented state and covariance 
+xa = [ x_hat; zeros(6,1) ]; 
+Pa = [ P, zeros(6); zeros(6), Q ]; 
 
 %% DYNAMICS PROPAGATION 
 
@@ -41,8 +63,8 @@ load problem3data.mat
 Sx = chol(Pa)'; 
 
 % build sigma points 
-lambda_x = a^2 * (nx + nv + k) - (nx + nv); 
-XX = build_SP(xa, Pa, nx, nv, lambda_x); 
+lambda_xv = a^2 * (nx + nv + k) - (nx + nv); 
+XX = build_SP(xa, Pa, nx, nv, lambda_xv); 
 
 % propagate sigma points 
 XX_prop = []; 
@@ -55,80 +77,24 @@ for i = 1:length(XX)
 end 
 
 % combine sigma points 
-[x_bar, P_bar] = combine_SP(nx, nv, lambda_x, a, b, XX_prop); 
-
-%     % determine weights 
-%     w_0m = lambda_x / (nx + nv + lambda_x); 
-%     w_im = 1 / (2*(nx + nv + lambda_x)); 
-%     w_0c = lambda_x / (nx + nv + lambda_x) + 1 - a^2 + b; 
-%     w_ic = w_im; 
-%     
-%     N_SP = 2*(nx+nv) + 1; 
-% 
-%     % use predicted sigma points to calculate predicted state x_bar 
-%     x_bar = zeros(1, nx); 
-%     for i = 1 : N_SP
-% 
-%         if i == 1;      wx = w_0m; 
-%         else;           wx = w_im; 
-%         end
-% 
-%         % build xa_bar 
-%         xi_bar = wx * XX_prop(i, 1:nx); 
-%         x_bar = x_bar + xi_bar; 
-% 
-%     end  
-% 
-%     % predict Pa_bar 
-%     P_bar = zeros(nx); 
-%     for i = 1 : N_SP
-% 
-%         if i == 1;      wP = w_0c; 
-%         else;           wP = w_ic; 
-%         end
-% 
-%         % build Pa_bar 
-%         xtilde = [ XX_prop(i, 1:nx) - x_bar ]'; 
-%         Pi_bar = wP * (xtilde) * (xtilde)'; 
-%         P_bar = P_bar + Pi_bar; 
-% 
-%     end 
+[x_bar, P_bar] = combine_SP(nx, nv, lambda_xv, a, b, XX_prop); 
 
 %% MEASUREMENT UPDATE 
 
 % push sigma points through measurement model. Use min bearing, max
 % bearing, and min range 
-i    = 1; 
-zi   = lidar(i).z; 
-z_ri = zi(:,1);     % range 
-z_bi = zi(:,2);     % bearing 
-z    = [ min(z_bi); max(z_bi); min(z_ri) ]; 
+zj   = lidar(j).z; 
+z_rj = zj(:,1);     % range 
+z_bj = zj(:,2);     % bearing 
+z    = [ min(z_bj); max(z_bj); min(z_rj) ]; 
 
 % stack states and covariance 
 xa_bar = [x_bar; zeros(nz,1)]; 
 Pa_bar = [P_bar, zeros(nx, nz); zeros(nz, nx), R]; 
 
 % build sigma points 
-lambda_z = a^2 * (nx + nz + k) - (nx + nz); 
-% XX_bar = build_SP(xa_bar, Pa_bar, nz, nx, lambda_z); 
-
-    % cholesky factorize Pa 
-    Sx_bar = chol(Pa_bar)'; 
-
-    % build sigma points. REMEMBER: there will be 2*(nx+nv)+1 sigma points 
-    XX_bar(1,:) = xa_bar'; 
-    for i = 1 : nx + nz 
-
-        XXi = xa_bar' + sqrt( nx + nz + lambda_z ) * Sx_bar(:,i)'; 
-        XX_bar  = [ XX_bar; XXi ]; 
-
-    end 
-    for i = nx + nz + 1 : 2*(nx + nz)
-
-        XXi = xa_bar' - sqrt( nx + nz + lambda_z) * Sx_bar(:, i - nx - nz)'; 
-        XX_bar  = [ XX_bar; XXi ]; 
-
-    end 
+lambda_xz = a^2 * (nx + nz + k) - (nx + nz); 
+XX_bar = build_SP(xa_bar, Pa_bar, nz, nx, lambda_xz); 
 
 % push sigma points through measurement model 
 ZZ_bar = []; 
@@ -137,33 +103,28 @@ for i = 1:length(XX_bar)
     ZZ_bar   = [ ZZ_bar; ZZ_i_bar' ]; 
 end 
 
-% determine weights 
-w_0m = lambda_z / (nx + nz + lambda_z); 
-w_im = 1 / (2*(nx + nz + lambda_z)); 
-w_0c = lambda_z / (nx + nz + lambda_z) + 1 - a^2 + b; 
-w_ic = w_im; 
+% combine sigma points 
+[z_bar, Pzz] = combine_SP(nz, nx, lambda_xz, a, b, ZZ_bar); 
 
-N_SP = 2*(nx+nz) + 1; 
+% calculate Pxz 
+Pxz = calc_Pxz(nx, nz, lambda_xz, a, b, XX_bar, x_bar, ZZ_bar, z_bar); 
 
-% use predicted sigma points to calculate predicted state x_bar 
-z_bar = zeros(1, nz); 
-for i = 1 : N_SP
+% LMMSE update!!! 
+x_hat = x_bar + Pxz * Pzz^(-1) * [z - z_bar]; 
+P     = P_bar - Pxz * Pzz^(-1) * Pxz'; 
 
-    if i == 1;      wz = w_0m; 
-    else;           wz = w_im; 
-    end
+% save state and covariance
+x_hat_arr = [x_hat_arr; x_hat']; 
+P_cell{j+1} = {P}; 
 
-    % build xa_bar 
-    zi_bar = wz * ZZ_bar(i, 1:nz); 
-    z_bar = z_bar + zi_bar; 
+% update plot 
+plotcar(x_hat, '-', h)
 
 end 
-z_bar = z_bar'; 
 
-% combine sigma points 
-[z_bar, Pzz_bar] = combine_SP(nz, nx, lambda_z, a, b, ZZ_bar); 
+%% ANALYSIS 
 
-
+load problem3truth.mat
 
 
 %% subfunctions 
@@ -190,7 +151,34 @@ function XX = build_SP(xa, Pa, nx, nv, lambda)
 
 end 
 
-function [x_bar, P_bar] = combine_SP(nx, nv, lambda, a, b, XX_prop) 
+function Pxz = calc_Pxz(nx, nz, lambda_xz, a, b, XX_bar, x_bar, ZZ_bar, z_bar)
+
+    % determine weights 
+    w_0m = lambda_xz / (nx + nz + lambda_xz); 
+    w_im = 1 / (2*(nx + nz + lambda_xz)); 
+    w_0c = lambda_xz / (nx + nz + lambda_xz) + 1 - a^2 + b; 
+    w_ic = w_im; 
+
+    Pxz = zeros(size(nx, nz)); 
+    N_SP = 2*(nx + nz) + 1; 
+    for i = 1 : N_SP
+
+        if i == 1;      wP = w_0c; 
+        else;           wP = w_ic; 
+        end
+
+        % build Pa_bar 
+        xtilde = [ XX_bar(i, 1:nx)' - x_bar ]; 
+        ztilde = [ ZZ_bar(i, 1:nz)' - z_bar ]; 
+
+        Pi  = wP * (xtilde) * (ztilde)'; 
+        Pxz = Pxz + Pi; 
+
+    end 
+
+end 
+
+function [x_bar, Pxx_bar] = combine_SP(nx, nv, lambda, a, b, XX_prop) 
 
     % determine weights 
     w_0m = lambda / (nx + nv + lambda); 
@@ -215,7 +203,7 @@ function [x_bar, P_bar] = combine_SP(nx, nv, lambda, a, b, XX_prop)
     end  
 
     % predict Pa_bar 
-    P_bar = zeros(nx); 
+    Pxx_bar = zeros(nx); 
     for i = 1 : N_SP
 
         if i == 1;      wP = w_0c; 
@@ -225,7 +213,7 @@ function [x_bar, P_bar] = combine_SP(nx, nv, lambda, a, b, XX_prop)
         % build Pa_bar 
         xtilde = [ XX_prop(i, 1:nx) - x_bar ]'; 
         Pi_bar = wP * (xtilde) * (xtilde)'; 
-        P_bar = P_bar + Pi_bar; 
+        Pxx_bar = Pxx_bar + Pi_bar; 
 
     end 
     
--- set8/codeAndDataForStudents/codeAndDataForStudents/plotcar.m
+++ set8/codeAndDataForStudents/codeAndDataForStudents/plotcar.m
@@ -28,6 +28,7 @@ if (nargin < 3)
 end
 
 figure(h);
+
 %calculate the car corners
 Rot = [cos(x(4)), -sin(x(4)); ...
        sin(x(4)), cos(x(4))];
@@ -37,10 +38,13 @@ fp = x(1:2) + Rot*[x(5); -x(6)/2];
 bd = x(1:2) + Rot*[0; x(6)/2];
 bp = x(1:2) + Rot*[0; -x(6)/2];
 
+cla; 
 hold on;
 plot([fd(1); fp(1)], [fd(2); fp(2)], pstr);
 plot([fd(1); bd(1)], [fd(2); bd(2)], pstr);
 plot([bd(1); bp(1)], [bd(2); bp(2)], pstr);
 plot([bp(1); fp(1)], [bp(2); fp(2)], pstr);
+
+pause(0.05)
 
 return;
